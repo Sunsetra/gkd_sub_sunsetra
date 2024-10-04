011ec3330ada02b2269586d69370db06f5da60c4
Sunsetra <xingxuanma@gmail.com>
2024-10-04 09:54:35 +0800

    fix(app): add 2 rules for nga

--- src/apps/gov.pianzong.androidnga.ts
+++ src/apps/gov.pianzong.androidnga.ts
@@ -6,7 +6,7 @@ export default defineGkdApp({
   groups: [
     {
       key: 0,
-      name: '全屏广告-关闭开屏广告',
+      name: '全屏广告-开屏广告',
       desc: '自动点击[跳过广告]',
       fastQuery: true,
       activityIds: '.activity.LoadingActivity',
@@ -14,12 +14,38 @@ export default defineGkdApp({
     },
     {
       key: 1,
-      name: '局部广告-关闭推荐收藏顶部广告',
+      name: '局部广告-推荐收藏顶部广告',
       desc: '自动点击[X]',
       fastQuery: true,
       activityIds: 'com.donews.nga.activitys.MainActivity',
       rules:
         '@ImageView[id="gov.pianzong.androidnga:id/iv_close_community_ad"]',
+    },
+    {
+      key: 2,
+      name: '局部广告-帖子回复底部广告',
+      desc: '自动点击[X]',
+      fastQuery: true,
+      activityIds: '.activity.forumdetail.ArticleDetailActivity',
+      rules: [
+        {
+          position: {
+            right: 'width*0.0407',
+            top: 'width*0.1809',
+          },
+          matches:
+            '@TextView[index=parent.childCount.minus(1)] <n WebView[text="NGA帖子页"]',
+        },
+      ],
+    },
+    {
+      key: 3,
+      name: '局部广告-帖子列表广告',
+      desc: '自动点击[X]',
+      fastQuery: true,
+      activityIds: '.activity.forumdetail.ArticleDetailActivity',
+      rules:
+        '@ImageView[parent.childCount=1] <<n FrameLayout[id="gov.pianzong.androidnga:id/ksad_container"]',
     },
   ],
 });
